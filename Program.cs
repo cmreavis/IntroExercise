bb013c49cca988b66a97daa0c18d4b33a0b25731
cmreavis <cmreavis97@gmail.com>
2022-09-09 12:38:54 -0400

    Fixed

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Thank you, John!");
+Console.WriteLine("Thank you, Andrea!");
+Console.WriteLine("Thank you, Miah!");
